8fea0c43a6c90623ae4da95c5c706e8d8672c230
John Nelson <jrnelson75032@gmail.com>
2025-11-06 00:37:50 -0600

    ATM simulator implements check balance, deposit, and withdraw.

--- src/com/simple/atm/ATM.java
+++ src/com/simple/atm/ATM.java
@@ -1,4 +1,20 @@
 package com.simple.atm;
 
 public class ATM {
+    public  ATM() {
+        System.out.print("Welcome! Please enter your pin: ");
+    }
+
+    public void menu() {
+        System.out.println("\n1.  Check Balance");
+        System.out.println("\n2.  Deposit");
+        System.out.println("\n3.  Withdraw");
+        System.out.println("\n4.  Exit");
+    }
+
+    public void checkBalance(double balance) { System.out.printf("Your balance is $%.2f ", balance);}
+    public void depositMenu() { System.out.println("Please enter amount to deposit.");}
+    public void withdrawMenu() { System.out.println("Please enter amount to withdraw.");}
+    public void invalidMenu() { System.out.println("Invalid option.  Please try again.");}
+    public void exitMenu() { System.out.println("THank you for banking with us! Have a nice day.");}
 }
--- src/com/simple/atm/Main.java
+++ src/com/simple/atm/Main.java
@@ -1,13 +1,47 @@
 package com.simple.atm;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Account account1 = new Account(5000.00, 1456);
-        System.out.println(account1.getBalance());
-        account1.deposit(1000);
-        System.out.println((account1.getBalance()));
-        account1.withdraw(10000);
-        account1.withdraw(1000);
-        System.out.println(account1.getBalance());
-    }
+        int pinNum;
+        int userOption = 0;
+
+        Account account1 = new Account(5000, 1423);
+
+        Scanner scanner = new Scanner(System.in);
+
+        ATM atm = new ATM();
+
+        pinNum = scanner.nextInt();
+
+        if(account1.validatePin((pinNum))) {
+            do {
+                atm.menu();
+                userOption = scanner.nextInt();
+                switch (userOption) {
+                    case 1:
+                        atm.checkBalance(account1.getBalance());
+                        break;
+                    case  2:
+                        atm.depositMenu();
+                        double depAmnt = scanner.nextDouble();
+                        account1.deposit(depAmnt);
+                        break;
+                    case 3:
+                        atm.withdrawMenu();
+                        double withdrawAmount = scanner.nextDouble();
+                        account1.withdraw(withdrawAmount);
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        atm.invalidMenu();
+
+                }            } while (userOption != 4);
+            atm.exitMenu();
+
+        }    }
 }
